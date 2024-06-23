b4b1b3bd93507840f15cfebf9901dba0166f0563
zDragon Paulo <23919@stu.ipbeja.pt>
2024-06-23 16:26:08 +0100

    Base de Dados alterada

--- src/AnimeContext.tsx
+++ src/AnimeContext.tsx
@@ -1,5 +1,11 @@
-import React, { createContext, useState, ReactNode, useContext, useEffect } from 'react';
-import { useUser } from './UserContext';
+import React, {
+  createContext,
+  useState,
+  ReactNode,
+  useContext,
+  useEffect,
+} from "react";
+import { useUser } from "./UserContext";
 
 interface Anime {
   mal_id: number;
@@ -36,19 +42,21 @@ export const AnimeContext = createContext<AnimeContextType>({
   lists: {
     porVer: [],
     aVer: [],
-    completado: []
+    completado: [],
   },
   saveListsToRestDB: () => {},
-  fetchAnimeLists: () => {}
+  fetchAnimeLists: () => {},
 });
 
-export const AnimeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
+export const AnimeProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
   const [animes, setAnimes] = useState<Anime[]>([]);
   const [searchPerformed, setSearchPerformed] = useState<boolean>(false);
   const [lists, setLists] = useState({
     porVer: [],
     aVer: [],
-    completado: []
+    completado: [],
   });
   const { user } = useUser();
 
@@ -60,15 +68,14 @@ export const AnimeProvider: React.FC<{ children: ReactNode }> = ({ children }) =
 
   const removeFromLists = (anime: Anime) => {
     return {
-      porVer: lists.porVer.filter(a => a.mal_id !== anime.mal_id),
-      aVer: lists.aVer.filter(a => a.mal_id !== anime.mal_id),
-      completado: lists.completado.filter(a => a.mal_id !== anime.mal_id),
+      porVer: lists.porVer.filter((a) => a.mal_id !== anime.mal_id),
+      aVer: lists.aVer.filter((a) => a.mal_id !== anime.mal_id),
+      completado: lists.completado.filter((a) => a.mal_id !== anime.mal_id),
     };
   };
- 
 
   const addToList = (anime: Anime, list: string) => {
-    setLists(prevLists => {
+    setLists((prevLists) => {
       const newList = removeFromLists(anime);
       newList[list].push(anime);
       return newList;
@@ -79,25 +86,25 @@ export const AnimeProvider: React.FC<{ children: ReactNode }> = ({ children }) =
       const response = await fetch(
         `https://myanimecollection-cdd2.restdb.io/rest/listasanimes?q={"id_utilizador":${userId}}`,
         {
-          method: 'GET',
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json',
-            'x-apikey': '6675a683be0bc8beb8eafe89'
-          }
+            "Content-Type": "application/json",
+            "x-apikey": "6675a683be0bc8beb8eafe89",
+          },
         }
       );
 
       if (!response.ok) {
-        throw new Error('Erro ao buscar listas de animes');
+        throw new Error("Erro ao buscar listas de animes");
       }
 
       const data = await response.json();
-      console.log('Listas de animes buscadas:', data);
+      console.log("Listas de animes buscadas:", data);
 
       const newLists = {
         porVer: [],
         aVer: [],
-        completado: []
+        completado: [],
       };
 
       data.forEach((list: any) => {
@@ -112,81 +119,107 @@ export const AnimeProvider: React.FC<{ children: ReactNode }> = ({ children }) =
 
       return newLists;
     } catch (error) {
-      console.error('Erro ao buscar listas de animes:', error);
+      console.error("Erro ao buscar listas de animes:", error);
       return {
         porVer: [],
         aVer: [],
-        completado: []
+        completado: [],
       };
     }
   };
 
   const saveListsToRestDB = async () => {
     if (!user || !user.id_utilizador) {
-      console.error('Usuário não está autenticado ou ID do usuário não está disponível');
+      console.error(
+        "Usuário não está autenticado ou ID do usuário não está disponível"
+      );
       return;
     }
-  
+
     const dataToSave = [
-      { id_lista: 1, id_utilizador: user.id_utilizador, lista_animes: lists.porVer },
-      { id_lista: 2, id_utilizador: user.id_utilizador, lista_animes: lists.aVer },
-      { id_lista: 3, id_utilizador: user.id_utilizador, lista_animes: lists.completado },
+      {
+        id_lista: 1,
+        id_utilizador: user.id_utilizador,
+        lista_animes: lists.porVer,
+      },
+      {
+        id_lista: 2,
+        id_utilizador: user.id_utilizador,
+        lista_animes: lists.aVer,
+      },
+      {
+        id_lista: 3,
+        id_utilizador: user.id_utilizador,
+        lista_animes: lists.completado,
+      },
     ];
-  
+
     try {
       for (const data of dataToSave) {
-        console.log(`Verificando lista com id_utilizador: ${user.id_utilizador} e id_lista: ${data.id_lista}`);
+        console.log(
+          `Verificando lista com id_utilizador: ${user.id_utilizador} e id_lista: ${data.id_lista}`
+        );
         const checkResponse = await fetch(
           `https://myanimecollection-cdd2.restdb.io/rest/listasanimes?q={"id_utilizador":${user.id_utilizador},"id_lista":${data.id_lista}}`,
           {
-            method: 'GET',
+            method: "GET",
             headers: {
-              'Content-Type': 'application/json',
-              'x-apikey': '6675a683be0bc8beb8eafe89'
+              "Content-Type": "application/json",
+              "x-apikey": "6675a683be0bc8beb8eafe89",
             },
           }
         );
-  
+
         if (!checkResponse.ok) {
           const errorText = await checkResponse.text();
-          console.error(`Erro ao verificar a lista: ${checkResponse.statusText} - ${errorText}`);
-          throw new Error(`Erro ao verificar a lista: ${checkResponse.statusText} - ${errorText}`);
+          console.error(
+            `Erro ao verificar a lista: ${checkResponse.statusText} - ${errorText}`
+          );
+          throw new Error(
+            `Erro ao verificar a lista: ${checkResponse.statusText} - ${errorText}`
+          );
         }
-  
+
         const existingLists = await checkResponse.json();
         console.log(`Listas existentes: ${JSON.stringify(existingLists)}`);
-  
-        let method = 'POST';
-        let url = 'https://myanimecollection-cdd2.restdb.io/rest/listasanimes';
+
+        let method = "POST";
+        let url = "https://myanimecollection-cdd2.restdb.io/rest/listasanimes";
         if (existingLists.length > 0) {
-          method = 'PATCH';
+          method = "PATCH";
           url = `https://myanimecollection-cdd2.restdb.io/rest/listasanimes/${existingLists[0]._id}`;
         }
-  
+
         const response = await fetch(url, {
           method: method,
           headers: {
-            'Content-Type': 'application/json',
-            'x-apikey': '6675a683be0bc8beb8eafe89'
+            "Content-Type": "application/json",
+            "x-apikey": "6675a683be0bc8beb8eafe89",
           },
-          body: JSON.stringify(data)
+          body: JSON.stringify(data),
         });
-  
+
         if (!response.ok) {
           const errorText = await response.text();
-          console.error(`Erro ao salvar a lista: ${response.statusText} - ${errorText}`);
-          throw new Error(`Erro ao salvar a lista: ${response.statusText} - ${errorText}`);
+          console.error(
+            `Erro ao salvar a lista: ${response.statusText} - ${errorText}`
+          );
+          throw new Error(
+            `Erro ao salvar a lista: ${response.statusText} - ${errorText}`
+          );
         }
         console.log(`Lista com id_lista: ${data.id_lista} salva com sucesso!`);
       }
     } catch (error) {
-      console.error('Erro ao salvar listas:', error);
+      console.error("Erro ao salvar listas:", error);
     }
   };
-  
+
   const fetchAnimeLists = async () => {
     if (!user || !user.id_utilizador) {
-      console.error('Usuário não está autenticado ou ID do usuário não está disponível');
+      console.error(
+        "Usuário não está autenticado ou ID do usuário não está disponível"
+      );
       return;
     }
 
@@ -194,25 +227,25 @@ export const AnimeProvider: React.FC<{ children: ReactNode }> = ({ children }) =
       const response = await fetch(
         `https://myanimecollection-cdd2.restdb.io/rest/listasanimes?q={"id_utilizador":${user.id_utilizador}}`,
         {
-          method: 'GET',
+          method: "GET",
           headers: {
-            'Content-Type': 'application/json',
-            'x-apikey': '6675a683be0bc8beb8eafe89'
-          }
+            "Content-Type": "application/json",
+            "x-apikey": "6675a683be0bc8beb8eafe89",
+          },
         }
       );
 
       if (!response.ok) {
-        throw new Error('Erro ao buscar listas de animes');
+        throw new Error("Erro ao buscar listas de animes");
       }
 
       const data = await response.json();
-      console.log('Listas de animes buscadas:', data);
+      console.log("Listas de animes buscadas:", data);
 
       const newLists = {
         porVer: [],
         aVer: [],
-        completado: []
+        completado: [],
       };
 
       data.forEach((list: any) => {
@@ -227,12 +260,24 @@ export const AnimeProvider: React.FC<{ children: ReactNode }> = ({ children }) =
 
       setLists(newLists);
     } catch (error) {
-      console.error('Erro ao buscar listas de animes:', error);
+      console.error("Erro ao buscar listas de animes:", error);
     }
   };
 
   return (
-    <AnimeContext.Provider value={{ animes, setAnimes, searchPerformed, setSearchPerformed, addToList, lists, saveListsToRestDB, fetchAnimeLists, fetchAnimeListsByUserId }}>
+    <AnimeContext.Provider
+      value={{
+        animes,
+        setAnimes,
+        searchPerformed,
+        setSearchPerformed,
+        addToList,
+        lists,
+        saveListsToRestDB,
+        fetchAnimeLists,
+        fetchAnimeListsByUserId,
+      }}
+    >
       {children}
     </AnimeContext.Provider>
   );
